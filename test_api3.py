b57245599922f9895166287d72e6c1dd86c78a62
atul25827 <atul.chune@merillife.com>
2026-08-18 16:56:26 +0530

    Revert "Merge branch 'club_new' of https://github.com/atul25827/academy-backend into club_new"
    This reverts commit a1eec726a069b994631630fc59a006c8e8ca7256, reversing
    changes made to 7de6253524a7cd320d61e023a859dd82b16b9089.

--- test_api3.py
+++
@@ -1,9 +0,0 @@
-﻿def run():
-    import frappe
-    frappe.set_user("Administrator")
-    from academy.api.booking import get_approver_booking_list
-    try:
-        res = get_approver_booking_list(search_name="test")
-        print("Success!", len(res.get('data', [])))
-    except Exception as e:
-        print("Error caught!", str(e))
